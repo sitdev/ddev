24736fd16513e0b35581e2733b1078a9491fd78c
Jeremy Strom <jstrom@situationinteractive.com>
2022-09-28 17:33:58 -0500

    Use temp dir instead of local

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "${1}"
 branch=${1:-main}
-git clone -b ${branch} git@github.com:sitdev/ddev.git .ddev-setup --quiet
-bash .ddev-setup/setup.sh
-rm -rf .ddev-setup
+export INSTALL_TMPDIR=$(mktemp -d)
+git clone -b ${branch} git@github.com:sitdev/ddev.git ${INSTALL_TMPDIR} --quiet
+bash "${INSTALL_TMPDIR}/setup.sh"
+rm -rf ${INSTALL_TMPDIR}
