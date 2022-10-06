ea6fdd047561246c058ca27f137e9ab83cc0c7dc
Jeremy Strom <jstrom@situationinteractive.com>
2022-10-06 12:49:53 -0500

    update install

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 branch=${1:-main}
 export INSTALL_TMPDIR="ddev-install"
-git clone -b ${branch} git@github.com:sitdev/ddev.git ${INSTALL_TMPDIR} --quiet &> /dev/null
+git clone -b ${branch} git@github.com:sitdev/ddev.git ${INSTALL_TMPDIR} --quiet
 bash "${INSTALL_TMPDIR}/setup.sh"
 rm -rf ${INSTALL_TMPDIR}
