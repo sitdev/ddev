6ff29bb4a470eac425b42877fae810a8906a6936
Jeremy Strom <jstrom@situationinteractive.com>
2023-02-28 09:40:37 -0600

    Add support for phpunit tests

---
+++ .ddev/homeadditions/config/phpunit/local-tests-config.php
@@ -0,0 +1,12 @@
+<?php
+/*
+This is a sample local-config.php file
+In it, you *must* include the three main database defines
+
+You may include other settings here for various environments
+*/
+
+define( 'DB_NAME', 'db_tests' );
+define( 'DB_USER', 'db' );
+define( 'DB_PASSWORD', 'db' );
+define( 'DB_HOST', 'db' );
